5d63dafd130248a8628883559998b699dd4bfc06
PodlinovaMaryna <podlinovamaryna@gmail.com>
2020-05-21 03:26:35 +0300

    add class show, hide and fade in style.css and add tabs in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () =>{
 
-    
+    const tabsContent = document.querySelectorAll(".tabcontent"),
+          tabs = document.querySelectorAll('.tabheader__item'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+
+        function hideTabContent () {
+            tabsContent.forEach(item =>{
+                item.classList.add('hide');
+                item.classList.remove('show');
+
+            });
+            tabs.forEach(item =>{
+                  item.classList.remove('tabheader__item_active');
+            });
+        }
+
+        function showTabContent (i = 0){
+            tabsContent[i].classList.add('show');
+            tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+            
+        }
+        hideTabContent();
+        showTabContent();
+        tabsParent.addEventListener('click', (event) =>{
+            const target = event.target;
+            if(target && target.classList.contains('tabheader__item')){
+                tabs.forEach((item, i) =>{
+                    if(target == item){
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                });
+            }
+        });
 
 
 
