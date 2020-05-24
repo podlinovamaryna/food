0bf585a46a98e7159a525c7c457204e43f9602a2
PodlinovaMaryna <podlinovamaryna@gmail.com>
2020-05-25 01:37:40 +0300

    add data-modal and data-clise in HTML and add modal in scrips.js

--- js/script.js
+++ js/script.js
@@ -90,5 +90,40 @@ function setClock(selector, endTime){
 }
 
 setClock('.timer', deadLine);
+
+//modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+      modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () =>{
+             modal.classList.add('show');
+             modal.classList.remove('hide');
+             document.body.style.overflow = "hidden";
+        });
+      });
+
+      function closeModal(){
+           modal.classList.add('hide');
+           modal.classList.remove('show');
+           document.body.style.overflow = "";
+      }
+
+      modalCloseBtn.addEventListener('click', closeModal);
+
+      modal.addEventListener('click', (e) => {
+        if(e.target === modal){
+            closeModal();
+        }
+      });
+
+      document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape'){
+            closeModal();
+        }
+      });
+
 });
 
