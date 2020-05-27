888459d53aa4bf90e5bbba6686d082e723466789
PodlinovaMaryna <podlinovamaryna@gmail.com>
2020-05-27 03:20:16 +0300

    change getZero and render(add classes to element

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () =>{
 
 //Timer
 
-const deadLine = '2020-05-25';
+const deadLine = '2020-05-27';
 
 function getTimeRemaining(endTime){
     const t = Date.parse(endTime) - Date.parse(new Date()),
@@ -63,6 +63,8 @@ function getTimeRemaining(endTime){
 function getZero(num){
     if(num >= 0 && num < 10){
         return `0${num}`;
+    }else if(num < 0){
+        return '00';
     }else{
         return num;
     }
@@ -101,7 +103,7 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
             modal.classList.add('show');
             modal.classList.remove('hide');
             document.body.style.overflow = "hidden";
-            clearInterval(modalTimerId);
+          //  clearInterval(modalTimerId);
       }
       modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
@@ -127,7 +129,7 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         }
       });
 
-      const modalTimerId = setInterval(openModal, 10000);
+      //const modalTimerId = setInterval(openModal, 10000);
 
       function showModalByScroll(){
           if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -137,5 +139,71 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
       }
 
       window.addEventListener('scroll', showModalByScroll);
+// add menu cards
+      class menuCard{
+
+        constructor(src, alt, title, descr, price, parentSelector, ...classes){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render(){
+            const element = document.createElement('div');
+            if(this.classes.length === 0){
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            }else{
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            element.innerHTML = `
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        `;
+        this.parent.append(element);
+        }
+      }
+      new menuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+      ).render();
+
+      new menuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Премиум"',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
+      ).render();
+
+      new menuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+      ).render();
 });
 
