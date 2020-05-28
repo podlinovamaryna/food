2c916ec23f386f80258da29b0350e85c216a0ac9
PodlinovaMaryna <podlinovamaryna@gmail.com>
2020-05-28 03:07:44 +0300

    Forms, send info to server with general format

--- js/script.js
+++ js/script.js
@@ -205,5 +205,51 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
         21,
         '.menu .container',
       ).render();
+
+      //Forms, send info to server with general format
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: "Загрузка...",
+            success: "Спасибо! Скоро мы с Вами свяжемся",
+            failure: "Что-то пошло не так..."
+        };
+
+        forms.forEach(item => {
+            postData(item);
+        });
+
+        function postData(form){
+            form.addEventListener('submit',(e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+
+                //request.setRequestHeader('Content-type', 'multipart/form-data');
+                const formData = new FormData(form);
+                request.send(formData);
+
+                request.addEventListener('load', () => {
+                    if(request.status === 200){
+                        console.log(request.response);
+                        statusMessage.textContent = message.success;
+                        form.reset();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 2000);
+                    }else{
+                        statusMessage.textContent = message.failure;
+                    }
+                });
+
+            });
+        }
+
 });
 
