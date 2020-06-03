4edd29fbc90eee8d055dcc9867582bbea08e4421
PodlinovaMaryna <podlinovamaryna@gmail.com>
2020-06-04 02:49:38 +0300

    add db.json, npm, and change script.js- (add thanks modal, and fetch)

--- js/script.js
+++ js/script.js
@@ -96,8 +96,8 @@ setClock('.timer', deadLine);
 //modal
 
 const modalTrigger = document.querySelectorAll('[data-modal]'),
-      modal = document.querySelector('.modal'),
-      modalCloseBtn = document.querySelector('[data-close]');
+      modal = document.querySelector('.modal');
+      
 
       function openModal(){
             modal.classList.add('show');
@@ -115,10 +115,10 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
            document.body.style.overflow = "";
       }
 
-      modalCloseBtn.addEventListener('click', closeModal);
+      
 
       modal.addEventListener('click', (e) => {
-        if(e.target === modal){
+        if(e.target === modal|| e.target.getAttribute('data-close') == ""){
             closeModal();
         }
       });
@@ -228,34 +228,55 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
                 statusMessage.textContent = message.loading;
                 form.append(statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-
-                request.setRequestHeader('Content-type', 'application/json');
-
                 const formData = new FormData(form);
                 const object = {};
                 formData.forEach(function(value, key){
                     object[key] = value;
                 });
-                const json = JSON.stringify(object);
-                request.send(json);
-
-                request.addEventListener('load', () => {
-                    if(request.status === 200){
-                        console.log(request.response);
-                        statusMessage.textContent = message.success;
-                        form.reset();
-                        setTimeout(() => {
-                            statusMessage.remove();
-                        }, 2000);
-                    }else{
-                        statusMessage.textContent = message.failure;
-                    }
+               
+                fetch('server.php', {
+                    method: 'POST',
+                    headers: {'Content-type': 'application/json'},
+                    body: JSON.stringify(object)
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal( message.success);
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.failure);
+                })
+                .finally(() => {
+                    form.reset();
                 });
 
             });
         }
 
+        function showThanksModal(message){
+            const prevModalDialog = document.querySelector('.modal__dialog');
+
+            prevModalDialog.classList.add('hide');
+            openModal();
+
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `<div class = "modal__content">
+             <div class = "modal__close" data-close>×</div>
+             <div class = "modal__title">${message}</div>
+            </div>`;
+            document.querySelector('.modal').append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                prevModalDialog.classList.add('show');
+                prevModalDialog.classList.remove('hide');
+                closeModal();
+            }, 4000);
+        }
+        fetch('db.json')
+            .then(data => data.json())
+            .then(res => console.log(res));
 });
 
